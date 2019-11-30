b3ad6f30975d98444a849b3e45eaf9059841fb6a
Antonello Contini <antonello.contini@outlook.it>
2019-11-30 20:55:07 +0100

    definito set_partition per online

--- src/logistic_sim/include/OnlineTaskPlanner.hpp
+++ src/logistic_sim/include/OnlineTaskPlanner.hpp
@@ -1,9 +1,98 @@
 #pragma once
 
+#include<boost/thread.hpp>
 #include "TaskPlanner.hpp"
 
 namespace onlinetaskplanner
 {
+
+using t_coalition = std::pair<std::vector<logistic_sim::Mission>, logistic_sim::Mission>;
+
+struct less_V
+{
+    inline bool operator()(const t_coalition &A, const t_coalition &B)
+    {
+        return A.second.V < B.second.V;
+    }
+};
+
+void print_coalition(const t_coalition &coalition)
+{
+    auto tasks = coalition.first;
+    auto mission = coalition.second;
+    c_print("Mission id: ", mission.ID, green, P);
+    // std::cout << "pd: " << mission.PATH_DISTANCE << "\n";
+    std::cout << "td: " << mission.TOT_DEMAND << "\n";
+    // std::cout << " V: " << mission.V << "\n";
+    // auto size_dsts = mission.DSTS.size();
+    // std::cout << "dsts"
+    //           << "\n";
+    // for (int i = 0; i < size_dsts; i++)
+    // {
+    //     std::cout << mission.DSTS[i] << " ";
+    // }
+    // std::cout << "\n";
+
+    auto size_boh = mission.DEMANDS.size();
+    std::cout << "demands"
+              << "\n";
+    for (int i = 0; i < size_boh; i++)
+    {
+        std::cout << mission.DEMANDS[i] << " ";
+    }
+    std::cout << "\n";
+
+    // auto size_route = mission.ROUTE.size();
+    // std::cout << "route"
+    //           << "\n";
+    // for (int i = 0; i < size_route; i++)
+    // {
+    // std::cout << mission.ROUTE[i] << " ";
+    // }
+    // std::cout << "\n";
+
+    // c_print("tasks", magenta, P);
+
+    auto size_tasks = tasks.size();
+
+    for (auto i = 0; i < size_tasks; i++)
+    {
+        auto t = tasks[i];
+        c_print("task id: ", t.ID, magenta, P);
+        //     std::cout << "pd: " << t.PATH_DISTANCE << "\n";
+        std::cout << "td: " << t.TOT_DEMAND << "\n";
+        //     std::cout << " V: " << t.V << "\n";
+            auto size_dsts = t.DSTS.size();
+            std::cout << "dsts"
+                      << "\n";
+            for (int i = 0; i < size_dsts; i++)
+            {
+                std::cout << t.DSTS[i] << " ";
+            }
+            std::cout << "\n";
+
+        auto size_boh = t.DEMANDS.size();
+        std::cout << "demands"
+                  << "\n";
+        for (int i = 0; i < size_boh; i++)
+        {
+            std::cout << t.DEMANDS[i] << " ";
+        }
+        std::cout << "\n";
+
+        //     auto size_route = t.ROUTE.size();
+        //     std::cout << "route"
+        //               << "\n";
+        //     for (int i = 0; i < size_route; i++)
+        //     {
+        //         std::cout << t.ROUTE[i] << " ";
+        //     }
+        //     std::cout << "\n";
+    }
+
+    c_print("fine mission id: ", mission.ID, red, P);
+}
+
 
 const std::string PS_path = ros::package::getPath("logistic_sim");
 
@@ -14,8 +103,11 @@ public:
     void init(int argc, char **argv);
     void run();
     void token_callback(const logistic_sim::TokenConstPtr &msg) override;
+    std::vector<logistic_sim::Mission> set_partition(const std::vector<logistic_sim::Mission> &ts);
 protected:
-
+    int window_size = 9;
+    std::vector<std::vector<logistic_sim::Mission>> mission_windows;
+    boost::mutex window_mutex;
 };
 
 } // namespace onlinetaskplanner
--- src/logistic_sim/src/OnlineTaskPlanner.cpp
+++ src/logistic_sim/src/OnlineTaskPlanner.cpp
@@ -14,7 +14,7 @@ OnlineTaskPlanner::OnlineTaskPlanner(ros::NodeHandle &nh_, const std::string &na
 void OnlineTaskPlanner::init(int argc, char **argv)
 {
     srand(time(NULL));
-    chdir(PS_path.c_str());
+    int cmd_result = chdir(PS_path.c_str());
     mapname = string(argv[1]);
     std::string graph_file = "maps/" + mapname + "/" + mapname + ".graph";
     dimension = GetGraphDimension(graph_file.c_str());
@@ -70,6 +70,7 @@ void OnlineTaskPlanner::init(int argc, char **argv)
     token.ID_SENDER = TASK_PLANNER_ID;
     token.ID_RECEIVER = 0;
     token.INIT = true;
+    token.NEW_MISSIONS_AVAILABLE = false;
 
     pub_token.publish(token);
     ros::spinOnce();
@@ -81,14 +82,32 @@ void OnlineTaskPlanner::init(int argc, char **argv)
 
 void OnlineTaskPlanner::run()
 {
-    while(ros::ok())
+    std::cout << "Genero finestre di task" << std::endl;
+    while (!missions.empty())
     {
         /*
-         * in questo loop creo le tranche di task multi-item
+         * in questo loop creo le finestre di task multi-item
          * da inserire nel token, ogni task dovrebbe indicare
-         * a quale tranche appartiene in modo da non intervallarli
+         * a quale finestra appartiene in modo da non intervallarli
          */
+        auto first_it = missions.begin();
+        auto last_it = missions.end();
+        if (missions.size() >= window_size)
+        {
+            last_it = first_it + window_size;
+        }
+        std::vector<logistic_sim::Mission> tasks(first_it, last_it);
+        missions.erase(first_it, last_it);
+        std::vector<logistic_sim::Mission> window = set_partition(tasks);
+        // il mutex è necessario perchè nella thread del token
+        // si accede al vettore delle finestre
+        window_mutex.lock();
+        mission_windows.push_back(window);
+        window_mutex.unlock();
     }
+
+    // una volta calcolata l'ultima finestra si attende la terminazione
+    ros::waitForShutdown();
 }
 
 void OnlineTaskPlanner::token_callback(const logistic_sim::TokenConstPtr &msg)
@@ -111,6 +130,92 @@ void OnlineTaskPlanner::token_callback(const logistic_sim::TokenConstPtr &msg)
      * cosa nuova con una nuova tranche i robot spenti devono essere
      * riabilitati
      */
+}
+
+std::vector<logistic_sim::Mission> OnlineTaskPlanner::set_partition(const std::vector<logistic_sim::Mission> &ts)
+{
+    c_print("Calculating partitions", green, P);
+    std::vector<t_coalition> good_partition;
+    try
+    {
+        int num_tasks = ts.size();
+        c_print(num_tasks);
+        partition::iterator it(num_tasks);
+        static int id_partition = 0;
+
+        t_coalition candidate;
+        while (true)
+        {
+            std::vector<std::vector<logistic_sim::Mission>> partitions = *it[ts];
+            auto n_subsets = it.subsets();
+            logistic_sim::Mission candidate_partition;
+            candidate_partition.ID = id_partition;
+            // c_print(id_partition);
+            id_partition++;
+            int id_subset = 0;
+            double V = 0;
+            candidate.second = candidate_partition;
+            std::vector<logistic_sim::Mission> m;
+            for (int i = 0; i < n_subsets; i++)
+            {
+                std::vector<logistic_sim::Mission> subset = partitions[i];
+                logistic_sim::Mission candidate_subset;
+                candidate_subset.ID = id_subset;
+                id_subset++;
+                for (int j = 0; j < subset.size(); j++)
+                {
+                    candidate_subset.TOT_DEMAND += subset[j].TOT_DEMAND;
+                    copy(subset[j].DEMANDS.begin(), subset[j].DEMANDS.end(), back_inserter(candidate_subset.DEMANDS));
+                    copy(subset[j].DSTS.begin(), subset[j].DSTS.end(), back_inserter(candidate_subset.DSTS));
+                    copy(subset[j].ITEM.begin(), subset[j].ITEM.end(), back_inserter(candidate_subset.ITEM));
+                }
+
+                // calcolo la lunghezza del percorso di questa missione
+                // per poter stimare la metrica V
+                std::vector<uint> path;
+                for(auto it = candidate_subset.DSTS.begin(); it+1 != candidate_subset.DSTS.end(); it++)
+                {
+                    int dijkstra_result[64];
+                    uint dijkstra_size;
+                    dijkstra(*it, *(it+1), dijkstra_result, dijkstra_size, vertex_web, dimension);
+                    path.insert(path.end(), dijkstra_result, dijkstra_result+dijkstra_size);
+                }
+                candidate_subset.PATH_DISTANCE = compute_cost_of_route(path);
+                candidate_subset.V = (double) candidate_subset.PATH_DISTANCE / (double) candidate_subset.TOT_DEMAND;
+
+                candidate.second.V += candidate_subset.V;
+
+                if (candidate_subset.TOT_DEMAND > TEAM_CAPACITY)
+                {
+                    candidate.second.GOOD++;
+                }
+                // uso PICKUP == true se non è buona
+                // prima soglio e controllo che la partizione sia composta da sottoinsiemi processabili dai robot poi calcolo il
+                // percorso e metto in ordine per V poi prendo la prima buona
+                m.push_back(candidate_subset);
+            }
+
+            ++it;
+            candidate.first = m;
+            if (candidate.second.GOOD == 0)
+            {
+                // c_print("ok", green);
+                // print_coalition(*it);
+                good_partition.push_back(candidate);
+            }
+        }
+    }
+    catch (std::overflow_error &)
+    {
+    }
+
+    std::sort(good_partition.begin(), good_partition.end(), less_V());
+
+    auto ele = good_partition.front();
+
+    print_coalition(ele);
+
+    return ele.first;
 }
 
 } // namespace onlinetaskplanner
